c9e786140ef494564556aefa1f2eee4d70c96d0a
sochoeun khorn <sochoeun1202@gmail.com>
2024-07-17 16:29:17 +0700

    Build the generic fetching data

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,25 +5,23 @@ import CardGameSkeleton from "./CardGameSkeleton";
 import CardGameContainer from "./CardGameContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        padding={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10} padding={10}>
+        {/* Skeleton Card */}
         {isLoading &&
           skeletons.map((skeleton) => (
-            <CardGameContainer>
+            <CardGameContainer key={skeleton}>
               <CardGameSkeleton />
             </CardGameContainer>
           ))}
-        {games.map((game) => (
-          <CardGameContainer>
-            <CardGame game={game} key={game.id} />
+        {/* Game Card */}
+        {data.map((game) => (
+          <CardGameContainer key={game.id}>
+            <CardGame game={game} />
           </CardGameContainer>
         ))}
       </SimpleGrid>
